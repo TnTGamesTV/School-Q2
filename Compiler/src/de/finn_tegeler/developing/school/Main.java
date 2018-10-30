175ee4d542d842204ed897d8c83c0ecc85632067
finteg030901 <finteg030901@CG-TS03.couven.intern>
2018-10-30 12:45:01 +0100

    Initial commit

---
+++ Compiler/src/de/finn_tegeler/developing/school/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package de.finn_tegeler.developing.school;
+
+/**
+ * @author TnTGamesTV
+ * Project: Compiler
+ * Date: 30-10-2018
+ */
+public class Main {
+
+	/**
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
